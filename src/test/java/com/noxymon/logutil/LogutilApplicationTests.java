897df583a0399f610cedeaad69c72d64325050fd
noxymon <avicienna.ulhaq@gmail.com>
2019-01-10 00:12:03 +0700

    Change port to 8383

---
+++ src/test/java/com/noxymon/logutil/LogutilApplicationTests.java
@@ -0,0 +1,17 @@
+package com.noxymon.logutil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogutilApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
