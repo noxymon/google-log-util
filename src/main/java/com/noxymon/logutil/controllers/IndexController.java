22e7674120ad331d86638aeff4e201ae1667346a
noxymon <avicienna.ulhaq@gmail.com>
2019-01-11 18:12:10 +0700

    Change logutil to separated library

--- src/main/java/com/noxymon/logutil/controllers/IndexController.java
+++ src/main/java/com/noxymon/logutil/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.noxymon.logutil.controllers;
 
-import com.noxymon.logutil.utils.LogUtil;
+import com.google.cloud.logging.Severity;
+import id.alfadigital.lunar.LunarLogger;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +14,24 @@ public class IndexController {
 
     @RequestMapping("/log")
     public HttpEntity test(){
-        LogUtil.withLogName("test-app")
-                .info("testing blablabsclasjadhsakdda");
+        LunarLogger lunarLogger = new LunarLogger();
+        lunarLogger.logName("test-sena")
+                .withClass(this.getClass())
+                .withLevel(Severity.INFO)
+                .log("ini testing biasa");
+        try {
+            testError();
+        } catch (RuntimeException ex) {
+            lunarLogger.logName("test-sena")
+                    .withClass(this.getClass())
+                    .withLevel(Severity.INFO)
+                    .trace(ex.getMessage(), ex);
+        }
         return new ResponseEntity(null, HttpStatus.ACCEPTED);
+    }
+
+    private void testError() throws RuntimeException {
+        throw new RuntimeException("ajdgjsdhgajsdgajhdgsajdgjhagsdjad");
     }
 
 }
